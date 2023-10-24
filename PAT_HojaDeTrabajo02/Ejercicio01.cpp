5187748dd3baf63a022490ff71b6d32f20306352
josesinbob <112579166+josesinbob@users.noreply.github.com>
2023-10-23 22:39:08 -0600

    Update Ejercicio01.cpp
    ejercicio1

--- PAT_HojaDeTrabajo02/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio01.cpp
@@ -2,5 +2,34 @@
 
 Node<char>* Ejercicio01::rotateRight(Node<char>* head, int k)
 {
+     if (!head || k == 0) {
+        return head;
+    }
+
+    int length = 1;
+    Node<char>* tail = head;
+
+    while (tail->next) {
+        tail = tail->next;
+        length++;
+    }
+
+    k = k % length;
+
+    if (k == 0) {
+        return head;
+    }
+
+    tail->next = head;
+
+    int shift = length - k;
+    while (shift--) {
+        head = head->next;
+        tail = tail->next;
+    }
+
+    tail->next = nullptr;
+
+    return head;
     return nullptr;
 }
